d2d0f675facad2bb9660c0f993a16c1f1357f49e
HN <hhnguyen92@gmail.com>
2018-06-25 14:32:37 -0700

    Add seed data

--- populatedb.js
+++ populatedb.js
@@ -3,8 +3,10 @@ const Workspace = require('./models/workspace');
 
 
 var sample = new Workspace({team_id: '1', access_token: '2'});
+var sample2 = new Workspace({team_id: '4', access_token: '5'});
 
 sample.save();
+sample2.save();
   // sample.save(function (err) {
   //   if (err) {
   //     console.log(err);
