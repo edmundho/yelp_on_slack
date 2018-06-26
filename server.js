c6c6a58e6549a7f175e13a8cf487ade1ae0dc811
Marshall <marshallycheng@gmail.com>
2018-06-26 11:23:54 -0700

    Add new dialog options

--- server.js
+++ server.js
@@ -113,47 +113,31 @@ app.post('/posttest', (req, res) => {
         callback_id: 'submit-form',
         submit_label: 'Submit',
         elements: [{
+          label: 'Search Term',
+          name: "search-term",
+          type: 'text',
+          placeholder: 'e.g. Japanese tapas'
+          },
+          {
             label: 'Price',
             type: 'select',
             name: 'price_list',
-            options: [{
-                label: "$",
-                value: "one$"
-              },
-              {
-                label: "$$",
-                value: "two$"
-              },
-              {
-                label: "$$$",
-                value: "three$"
-              },
-              {
-                label: '$$$$',
-                value: 'four$'
-              }
+            options: [
+              { label: "$",value: 1 },
+              { label: "$$", value: 2 },
+              { label: "$$$", value: 3 },
+              { label: '$$$$', value: 4 }
             ]
           },
           {
             label: 'Distance',
             type: 'select',
             name: 'distance_range',
-            options: [{
-                label: "0.5mi",
-                value: 0.5
-              },
-              {
-                label: "1.0mi",
-                value: 1.0
-              },
-              {
-                label: "1.5mi",
-                value: 1.5
-              },
-              {
-                label: "2.0mi",
-                value: 2.0
-              }
+            options: [
+              { label: "0.5mi",value: 0.5},
+              { label: "1.0mi",value: 1.0},
+              { label: "1.5mi",value: 1.5},
+              { label: "2.0mi",value: 2.0}
             ]
           },
           {
@@ -194,6 +178,7 @@ app.post('/interactive-component', (req, res) => {
       // default response so slack doesnt close our request
       res.send('');
       const data = {
+        
         channel: channel
       };
       axios.post('http://yelponslack.herokuapp.com/restaurants', data);
