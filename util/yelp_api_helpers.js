e39d278cd9fb142bf81bf05968c6d6d05a172b9f
Marshall <marshallycheng@gmail.com>
2018-06-27 11:37:27 -0700

    Move yelp logic from server.js to yelp-api-helper.js

--- util/yelp_api_helpers.js
+++ util/yelp_api_helpers.js
@@ -1,48 +1,72 @@
+const { IncomingWebhook } = require('@slack/client');
+
 const metersToMiles = (meters) => (meters * 0.0006).toFixed(1);
+
 const ICON_HASH = {
   0: ":fire:",
   1: ":thumbsup:",
   2: ":star:"
 };
 
+const buildRestaurantMessage = (restaurant, num) => (
+  {
+    "fields": [{
+      "title": "Type",
+      "value": restaurant.categories[0].title,
+      "short": true
+    },
+    {
+      "title": "Price",
+      "value": restaurant.price,
+      "short": true
+    },
+    {
+      "title": "Reviews",
+      "value": restaurant.review_count,
+      "short": true
+    },
+    {
+      "title": "Avg Rating",
+      "value": restaurant.rating,
+      "short": true
+    },
+    {
+      "title": "Distance (mi.)",
+      "value": metersToMiles(restaurant.distance),
+      "short": true
+    }, {
+      "title": `Vote with ${ICON_HASH[num]}`,
+      "value": '',
+      "short": true
+    }
+    ],
+    "title": restaurant.name,
+    "title_link": restaurant.url,
+    "thumb_url": restaurant.image_url
+  }
+);
+
+const restaurantMessage = (businesses, webHook) => {
+  const webHookUrl = new IncomingWebhook(webHook);
+  const test = {
+    "attachments": [
+      buildRestaurantMessage(businesses[0], 0),
+      buildRestaurantMessage(businesses[1], 1),
+      buildRestaurantMessage(businesses[2], 2)
+    ]
+  };
+
+  webHookUrl.send(test, function (err, res) {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Message successfully sent');
+    }
+  });
+};
+
 module.exports = {
   metersToMiles: (meters) => (meters * 0.0006).toFixed(1),
   milesToMeters: (miles) => (miles * 1609.34).toFixed(),
-  buildRestaurantMessage: (restaurant, num) => (
-    {
-      "fields": [{
-          "title": "Type",
-          "value": restaurant.categories[0].title,
-          "short": true
-        },
-        {
-          "title": "Price",
-          "value": restaurant.price,
-          "short": true
-        },
-        {
-          "title": "Reviews",
-          "value": restaurant.review_count,
-          "short": true
-        },
-        {
-          "title": "Avg Rating",
-          "value": restaurant.rating,
-          "short": true
-        },
-        {
-          "title": "Distance (mi.)",
-          "value": metersToMiles(restaurant.distance),
-          "short": true
-        }, {
-          "title": `Vote with ${ICON_HASH[num]}`,
-          "value": '',
-          "short": true
-        }
-      ],
-      "title": restaurant.name,
-      "title_link": restaurant.url,
-      "thumb_url": restaurant.image_url
-    }
-  )
+  restaurantMessage
 };
