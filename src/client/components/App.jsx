101b973ea0387f1a004a646134a529380ee70316
Ed <edmundho9@gmail.com>
2018-06-27 18:07:41 -0700

    add ubereats video

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -10,7 +10,6 @@ class App extends Component {
           srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
         </a>
       </div>
-
     );
   }
 }
