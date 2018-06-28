efc732b06d7babce2c9a0ee4cbe5418e83631a9c
Ed <edmundho9@gmail.com>
2018-06-28 09:40:12 -0700

    add map image of restaurant locations to restaurants message

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,27 +4,22 @@ class App extends Component {
   render() {
     return (
       <div className='whole-container'>
-      <div className ='header-container'>
-      
-      <div className='header'>
-        
-        <div>Yelp on Slack</div>
- 
-          <div> <a href="">Github</a>
-            <a href="">Linkedins</a>          
+        <div className ='header-container'>
+          <div className='header'>
+            <div>Yelp on Slack</div>
+            <div> <a href="">Github</a>
+              <a href="">Linkedins</a>          
+            </div>
           </div>
-      </div>
-      
-      </div>
-      <div className='content-container'>
-          <div>Logo</div>
-          <h1>Insert Yack Splash Page Stuff Here!!</h1>
-          <a href="https://slack.com/oauth/authorize?scope=incoming-webhook&client_id=387620765190.386684586035">
-            <img  alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
-              srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
-          </a>
-      </div>
-        
+        </div>
+        <div className='content-container'>
+            <div>Logo</div>
+            <h1>Insert Yack Splash Page Stuff Here!!</h1>
+            <a href="https://slack.com/oauth/authorize?scope=incoming-webhook&client_id=387620765190.386684586035">
+              <img  alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
+                srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+            </a>
+        </div>
       </div>
     );
   }
