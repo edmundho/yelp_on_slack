f0b26b7b59dbfc2595ea5dd98be83043dc93f2c5
HN <hhnguyen92@gmail.com>
2018-06-28 13:34:27 -0700

    Change background to purple gradient with slack, yelp logos

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -17,23 +17,28 @@ class App extends Component {
       </div>
       
       </div>
-      <div className='content-container'>
+      <div className='first-content-container'>
           <div>Logo</div>
-          <h1>Yelp, Slack Integration</h1>
+          <div className='header-text'>Yelp, Slack Integration</div>
           <div className='logo-plus'>
-            <img className='yelp-logo' src="http://www.itsbennycho.com/Pizza%20Factory/icons/yelp.png" alt=""/> 
-            <div>
+          
+            <img className='yelp-logo' src="https://res.cloudinary.com/nenry/image/upload/v1530208197/homepage-internet-yelp-web-page-screen-logo-icon.svg" alt=""/> 
+            <div className='plus'>
             +
             </div>
             <img className='slack-logo' src="http://res.cloudinary.com/nenry/image/upload/v1530205092/slack_256.png" alt=""/> 
 
           </div>
           <a href="https://slack.com/oauth/authorize?scope=incoming-webhook&client_id=387620765190.386684586035">
-            <img  alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
+            <img className='add-slack' alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
               srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
           </a>
       </div>
-        
+        <div className='second-content-container'>
+          <div className='what-text'>
+          What is Yelp on Slack?
+          </div>
+        </div>
       </div>
 
     );
