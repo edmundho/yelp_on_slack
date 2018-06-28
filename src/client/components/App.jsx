dddcd2d91cc46d7957938177f6eda6094fc41547
HN <hhnguyen92@gmail.com>
2018-06-28 10:07:47 -0700

    Add gradient background, yelp, slack logos

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -19,7 +19,15 @@ class App extends Component {
       </div>
       <div className='content-container'>
           <div>Logo</div>
-          <h1>Insert Yack Splash Page Stuff Here!!</h1>
+          <h1>Yelp, Slack Integration</h1>
+          <div className='logo-plus'>
+            <img className='yelp-logo' src="http://www.itsbennycho.com/Pizza%20Factory/icons/yelp.png" alt=""/> 
+            <div>
+            +
+            </div>
+            <img className='slack-logo' src="http://res.cloudinary.com/nenry/image/upload/v1530205092/slack_256.png" alt=""/> 
+
+          </div>
           <a href="https://slack.com/oauth/authorize?scope=incoming-webhook&client_id=387620765190.386684586035">
             <img  alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
               srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
